f45048f595e88d27a0bb51eb88c6196d581f929d
Manish <mansha99@gmail.com>
2023-08-06 10:36:42 +0530

    Removed unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-import LoginComponent from "@/components/auth/login";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="p-10 ">
-      
       <div className="p-2 text-lg">Hello NextJS</div>
     </main>
   );
